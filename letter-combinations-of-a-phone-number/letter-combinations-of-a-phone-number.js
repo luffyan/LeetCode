4f6ff7d2a4ed845aadd5a279126b0062e1d17e2f
luffyan <ohanran@gmail.com>
2021-09-25 23:19:28 -0700

    Time: 72 ms (73.40%), Space: 38.8 MB (46.22%) - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if(!digits.length) return [];
+    var map = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    }
+    
+    var result = [];
+    
+    var findCombos = function(str, idx) {
+        if(idx === digits.length) {
+            result.push(str);
+            return;
+        }
+        for(char of map[digits[idx]]) {
+            findCombos(str+char, idx+1);
+        }
+    }
+    
+    findCombos('', 0);
+    return result;
+};
