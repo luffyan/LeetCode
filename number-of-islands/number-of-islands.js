e4cb936cf2de2cddc69a1330e2a35e1a242782b0
luffyan <ohanran@gmail.com>
2021-10-01 11:13:31 -0700

    Time: 108 ms (37.99%), Space: 41.6 MB (63.63%) - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    if(grid.length === 0) {
-        return 0;
-    }
+   
     var num = 0;
-    var row = grid.length;
-    var col = grid[0].length;
-    for (var i = 0; i < row; i++) {
-        for (var j = 0; j < col; j++) {
+    var nrow = grid.length;
+    var ncol = grid[0].length;
+    for (var i = 0; i < nrow; i++) {
+        for (var j = 0; j < ncol; j++) {
              if(grid[i][j] === "1") {
                 num++;
                 sink(grid, i, j);
@@ -24,7 +22,7 @@ var sink = function(grid, row, col) {
     var nrow = grid.length;
     var ncolumn = grid[0].length;
     
-    if(row < 0 || col < 0 || row === nrow || col===ncolumn || grid[row][col] === "0") {
+    if(row < 0 || col < 0 || row === nrow || col=== ncolumn || grid[row][col] === "0") {
         return;
     }
     grid[row][col] = "0";
