f23b9b157fb1839d3ad86ce7b075f1a50ab35c65
luffyan <ohanran@gmail.com>
2021-10-09 16:13:45 -0700

    Time: 92 ms (83.65%), Space: 45.4 MB (86.73%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,17 +12,36 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    var set = new Set();
-    while(headA) {
-            set.add(headA)
-            headA = headA.next;
+//     var set = new Set();
+//     while(headA) {
+//             set.add(headA)
+//             headA = headA.next;
+        
+//     }
+//     while(headB) {
+//         if(set.has(headB)) {
+//             return headB;
+//         }
+//         headB = headB.next;
+//     }
+//     return null;
+    var p1 = headA;
+    var p2 = headB;
+    
+    while(p1 !== p2) {
+       
+        if (p1 === null) {
+            p1 = headB;
+        } else {
+            p1 = p1.next;
+        }
+        if (p2 === null) {
+            p2 = headA;
+        } else {
+            p2 = p2.next;
+        }
+        
         
     }
-    while(headB) {
-        if(set.has(headB)) {
-            return headB;
-        }
-        headB = headB.next;
-    }
-    return null;
+    return p1;
 };
