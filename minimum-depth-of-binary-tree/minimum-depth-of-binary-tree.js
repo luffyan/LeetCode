12330bdbe201187fae0790ebafb01556fb7586f5
luffyan <ohanran@gmail.com>
2021-10-18 23:43:53 -0700

    Time: 325 ms (35.51%), Space: 102.7 MB (30.10%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.js
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.js
@@ -12,14 +12,27 @@
  */
 var minDepth = function(root) {
     
-        if(!root) {
-            return 0;
+        // if(!root) {
+        //     return 0;
+        // }
+        // var left = minDepth(root.left);
+        // var right = minDepth(root.right);
+        // if(!root.left) return right+1;
+        // if(!root.right) return left+1;
+        // return Math.min(left, right) + 1;
+    let depth = 1
+    let queue = [root]
+    while (queue.length > 0) {
+        let len = queue.length
+        for (let i = 0; i < len; i++) {
+            let node = queue.shift()
+            if (node === null) return 0
+            if (node.left === null && node.right === null) return depth
+            if (node.left !== null) queue.push(node.left)
+            if (node.right !== null) queue.push(node.right)
         }
-        var left = minDepth(root.left);
-        var right = minDepth(root.right);
-        if(!root.left && !root.right) return 1;
-        if(!root.left) return right+1;
-        if(!root.right) return left+1;
-        return Math.min(left, right) + 1;
+        depth++
+    }
+    return depth
     
 };
