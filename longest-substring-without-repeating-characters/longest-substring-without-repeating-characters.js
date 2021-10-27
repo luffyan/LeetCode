067718740f2f08e482d1af6b4939bbcb82e638c2
luffyan <ohanran@gmail.com>
2021-10-27 13:55:39 -0700

    Time: 124 ms (65.53%), Space: 44.9 MB (40.87%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,26 +3,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    var map = {};
-    
     var left = 0;
     var right = 0;
+    var currLen = 0;
+    var maxLen = 0;
     
-    var res = 0;
-    while (right < s.length) {
-        var r = s.charAt(right);
-        map[r] ? map[r]++ : map[r] = 1
-        
-        while (map[r] > 1) {
-            var l = s.charAt(left);
-            map[l]--;
-            left++
-        }
-        
-        res = Math.max(res, right-left+1);
-        
+    var map = {};
+    
+    while(right < s.length) {
+        var currChar = s[right];
+        map[currChar] = map[currChar]+1 || 1;
         right++;
+        
+        while (map[currChar] > 1) {
+            map[s[left]]--;
+            left++;
+        }
+        currLen = right - left;
+        maxLen = Math.max(currLen, maxLen);
+        
     }
-    
-    return res;
+    return maxLen;
 };
