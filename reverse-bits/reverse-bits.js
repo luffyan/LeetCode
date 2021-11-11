59b696d371471958c77860cae544e1eaf6af0411
luffyan <ohanran@gmail.com>
2021-11-11 13:27:29 -0800

    Time: 88 ms (58.19%), Space: 40.6 MB (42.89%) - LeetHub

--- reverse-bits/reverse-bits.js
+++ reverse-bits/reverse-bits.js
@@ -3,11 +3,11 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-  let result = 0;
+    let result = 0;
     
     for(let i = 0; i < 32; i++) {    
         result <<= 1;  
-        result |= n & 1;  
+        result += n & 1;  
         n >>= 1;
     }
     
