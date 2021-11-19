a419d184a7a0de4080f79b57c59cdd8c9e97617f
luffyan <ohanran@gmail.com>
2021-11-19 01:14:00 -0800

    Time: 68 ms (96.55%), Space: 40.3 MB (50.47%) - LeetHub

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -22,13 +22,13 @@ var search = function(nums, target) {
         var mid = Math.floor(start + (end-start) / 2);
         if(target === nums[mid]) {
             return true;
-        } else if(nums[mid] >= nums[start]) {
+        } else if(nums[mid] > nums[start]) {// 此处的 >= 不能变为 > ???因为有重复的，如果没有就可以任意切换
             if(target >= nums[start] && target <= nums[mid]){
                 end = mid;
             } else {
                 start = mid;
             }
-        } else {
+        } else if(nums[mid] < nums[end]){
             if(target >= nums[mid] && target <= nums[end]) {
                 start = mid;
             } else {
