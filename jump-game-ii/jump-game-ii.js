5f266c4bdeeda4a9a7560a6ea11072db9c670ffc
luffyan <ohanran@gmail.com>
2021-11-26 00:38:48 -0800

    Time: 125 ms (47.44%), Space: 40.7 MB (79.73%) - LeetHub

---
+++ jump-game-ii/jump-game-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    var jumps = 0, currentJumpEnd = 0, farthest = 0;
+        for (var i = 0; i < nums.length - 1; i++) {
+            // we continuously find the how far we can reach in the current jump
+            farthest = Math.max(farthest, i + nums[i]);
+            // if we have come to the end of the current jump,
+            // we need to make another jump
+            if (i == currentJumpEnd) {
+                jumps++;
+                currentJumpEnd = farthest;
+            }
+        }
+        return jumps;
+};
