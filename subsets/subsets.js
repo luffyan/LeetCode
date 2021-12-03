83b9065d9be2c3e3bb6b2a3ed2170da60e2ef136
luffyan <ohanran@gmail.com>
2021-12-03 11:50:05 -0800

    Time: 84 ms (44.34%), Space: 41 MB (34.77%) - LeetHub

--- subsets/subsets.js
+++ subsets/subsets.js
@@ -4,15 +4,23 @@
  */
 var subsets = function(nums) {
     var result = [];
-    var backtrack = (nums, pos, list) => {
+    
+    if(nums === null || nums.length === 0 ){
+        return result;
+    }
+    
+    var findSubsets = (list, start) => {
         result.push(list.slice());
-
-        for(var i = pos; i < nums.length; i++) {
+        
+        for(var i = start ; i < nums.length; i++) {// i = start not 0!!! use start to control you keep choosing the next element to add to your list
             list.push(nums[i]);
-            backtrack(nums, i+1, list);
+            findSubsets(list, i+1);
             list.pop();
         }
     }
-    backtrack(nums, 0, []);
+    
+    findSubsets([], 0);
     return result;
 };
+
+
