9e7593a3f18241655054d83a51b58b539c4a74c0
luffyan <ohanran@gmail.com>
2021-12-04 02:21:00 -0800

    Time: 64 ms (96.42%), Space: 38.8 MB (33.90%) - LeetHub

--- decode-string/decode-string.js
+++ decode-string/decode-string.js
@@ -3,32 +3,30 @@
  * @return {string}
  */
 var decodeString = function(s) {
-    var result = '';
-    for (var char of s) {
-        var res = '';
-        var multi = 0;
-        var stack_multi = [];
-        var stack_res = [];
-        
-        for(var c of s) {
-            if(c >= '0' && c <= '9') {
-                multi = multi*10 + parseInt(c);
-            }else if(c === '[') {
-                stack_multi.push(multi);
-                stack_res.push(res);
-                multi = 0;
-                res = '';
-            }else if(c === ']') {
-                var temp = '';
-                var currMulti = stack_multi.pop();
-                for(var i = 0; i < currMulti; i++) {
-                    temp += res;
-                }
-                res = stack_res.pop() + temp;
-            }else {
-                res += c;
+    var stack_multi = [];
+    var stack_res = [];
+    var multi = 0;
+    var res = '';
+    
+    for(var char of s) {
+        if(char >= '0' && char <= '9') {
+            multi = multi * 10 + parseInt(char);
+        } else if (char === '[') {
+            stack_multi.push(multi);
+            stack_res.push(res);
+            res = '';
+            multi = 0;
+        } else if (char === ']') {
+            var temp = '';
+            var num = stack_multi.pop();
+            for(var i = 0; i < num; i++) {
+                temp += res;
             }
+            res = stack_res.pop() + temp;
+        } else {
+            res += char;
         }
+        
     }
     return res;
 };
