569430e24529de152bd4d0a9cf0d14af4e180ca1
luffyan <ohanran@gmail.com>
2021-12-05 22:18:32 -0800

    Time: 128 ms (20.34%), Space: 44.8 MB (8.95%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.js
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -3,31 +3,25 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    var start = [];
-    var end = [];
-    var i = 0;
-    var j = 0;
-    
+    intervals.sort((a,b) => (a[0]-b[0])); // 从小到大排序，是a-b
+    console.log(intervals);
     var count = 0;
-    var result = 0;
-    for(var interval of intervals) {
-        start.push(interval[0]);
-        end.push(interval[1]);
-    }
+
+    var pq = new MinPriorityQueue();
     
-    start.sort((a,b) =>a-b);
-    end.sort((a,b) =>a-b);
-
-    while(i < start.length){
-		if(start[i] < end[j]){
-			count++
-			i++
-		}else{
-			j++
-			count--
-		}
-		result=Math.max(count,result)
-	}
-
-	return result
-};
+    //add first end time to pq
+    pq.enqueue(intervals[0][1]);
+    count++;
+    //compare curr start time with first of queue
+    for(var i = 1; i < intervals.length; i++) {
+        var interval = intervals[i];
+        if(interval[0] < pq.front().element) {
+            count++;
+            pq.enqueue(interval[1]);
+        } else {
+            pq.dequeue().element;
+            pq.enqueue(interval[1]);
+        }
+    }
+    return count;
+}
