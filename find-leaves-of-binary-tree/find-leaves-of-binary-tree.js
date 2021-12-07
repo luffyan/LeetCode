fc7f8070b7d29622713877c7373aa60c1fb8aec3
luffyan <ohanran@gmail.com>
2021-12-07 01:42:40 -0800

    Time: 100 ms (16.92%), Space: 39.4 MB (91.65%) - LeetHub

--- find-leaves-of-binary-tree/find-leaves-of-binary-tree.js
+++ find-leaves-of-binary-tree/find-leaves-of-binary-tree.js
@@ -12,38 +12,34 @@
  */
 
 
+//想让如果都是leaves，就都一样的level，
+//那就从leave开始count， 就求高度，从下往上
 
 var findLeaves = function(root) {
-    var res = [];
+    var result = [];
     var findHeight = (node) => {
         if(node === null) {
             return -1;
         }
-        if(!node.left && !node.right) {
-            
-            if(!res[0]) {
-                res[0] = [];
-            }
-            res[0].push(node.val);
-            return 0;
-        }
-       
         var left = findHeight(node.left);
         var right = findHeight(node.right);
         
-        var height = Math.max(left, right) + 1;
+        var height = Math.max(left, right)+1;
+        if(!result[height]) {
+            result[height] = [];
+        } 
+        result[height].push(node.val);
         
-        if(!res[height]) {
-            res[height] = [];
-        }
-        res[height].push(node.val);
         return height;
     }
     findHeight(root);
-    return res;
+    return result;
+    
 }
 
 //----------------findDepth-------------------
+//depth就是在树是同一个level，大家的depth都一样
+
 // var findDepth = function(root, depth) {
 //     if(root === null) {
 //         return;
@@ -55,3 +51,23 @@ var findLeaves = function(root) {
 //     findDepth(root.left, depth+1);
 //     findDepth(root.right, depth+1);
 // };
+
+// var res = [];
+//     var findHeight = (node) => {
+//         if(node === null) {
+//             return -1;
+//         }
+       
+//         var left = findHeight(node.left);
+//         var right = findHeight(node.right);
+        
+//         var height = Math.max(left, right) + 1;
+        
+//         if(!res[height]) {
+//             res[height] = [];
+//         }
+//         res[height].push(node.val);
+//         return height;
+//     }
+//     findHeight(root);
+//     return res;
