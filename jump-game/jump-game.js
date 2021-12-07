89f1900205a487a1095b245fefa35d5943da417e
luffyan <ohanran@gmail.com>
2021-12-07 14:29:10 -0800

    Time: 167 ms (34.95%), Space: 43 MB (58.87%) - LeetHub

--- jump-game/jump-game.js
+++ jump-game/jump-game.js
@@ -5,32 +5,41 @@
 
 
 var canJump = function(nums) {
-    var map = {};
-    return canJumpFrom(0, nums, map);
-    
+    // var map = {};
+    // return canJumpFrom(0, nums, map);
+    var lastPos = nums.length - 1;
+    for(var i = lastPos; i >= 0; i--) {
+        if(nums[i] + i >= lastPos) {   
+            lastPos = i;
+        }
+    }
+    return lastPos === 0;
 };
 
+// DP = backtrack with memoization
+
 // can jump to the end of the array
-var canJumpFrom = (pos, nums, map) => {
-    if(pos === nums.length-1) {
-        return true;
-    }
-    if(map[pos] !== undefined) {
-        return map[pos];
-    }
+// var canJumpFrom = (pos, nums, map) => {
+//     if(pos === nums.length-1) {
+//         return true;
+//     }
+//     if(map[pos] !== undefined) {
+//         return map[pos];
+//     }
     
-    // furthest pos can be reached from curr pos
-    var furthest = Math.min(pos + nums[pos], nums.length-1);
+//     // furthest pos can be reached from curr pos
+//     var furthest = Math.min(pos + nums[pos], nums.length-1);
     
-    for(var i = pos + 1; i <= furthest; i++) {
-        if(map[i]) {
-            return true;
-        }
-        if(canJumpFrom(i, nums, map)) {
-            map[pos] = true;
-            return true;
-        }
-    }
-    map[pos] = false;
-    return false;
-}
+//     for(var i = furthest; i > pos; i--) {
+//         if(map[i]) {
+//             return true;
+//         }
+//         if(canJumpFrom(i, nums, map)) {
+//             map[pos] = true;
+//             return true;
+//         }
+//     }
+    
+//     map[pos] = false;
+//     return false;
+// }
