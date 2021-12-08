001d09c9fb5c93e67c66045da335c6be51def4b5
luffyan <ohanran@gmail.com>
2021-12-08 14:29:55 -0800

    Time: 90 ms (34.10%), Space: 42.8 MB (45.52%) - LeetHub

---
+++ path-sum/path-sum.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (root === null) return false;
+    return dfs(root, targetSum, 0);
+};
+
+var dfs = (root, targetSum, sum) => {
+    if(root === null) {
+        return false;
+    }
+    if(!root.left && !root.right) {   
+        return sum + root.val === targetSum;
+    } 
+    return dfs(root.left, targetSum, sum + root.val) ||
+       dfs(root.right, targetSum, sum + root.val);
+}
+
+// find all combo of sums
+// base case: sum === target
