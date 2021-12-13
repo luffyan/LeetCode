4d04add06d0f28dac60453b86b6ace478770b218
luffyan <ohanran@gmail.com>
2021-12-13 13:11:08 -0800

    Time: 84 ms (41.31%), Space: 40.7 MB (80.36%) - LeetHub

--- subsets/subsets.js
+++ subsets/subsets.js
@@ -2,16 +2,16 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// Input: nums = [1,2,3]  
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+//N* 2^N, N, build N number of substes, each subset has2^n combos
 var subsets = function(nums) {
     var result = [];
-    
     if(nums === null || nums.length === 0 ){
         return result;
     }
-    
     var findSubsets = (list, start) => {
-        result.push(list.slice());
-        
+        result.push(list.slice());//别忘了slice
         for(var i = start ; i < nums.length; i++) {// i = start not 0!!! use start to control you keep choosing the next element to add to your list
             list.push(nums[i]);
             findSubsets(list, i+1);
