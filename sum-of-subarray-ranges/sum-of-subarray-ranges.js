2c659572cf9e57b9be1970091602d87afc1e8e47
luffyan <ohanran@gmail.com>
2021-12-18 16:44:38 -0800

    Time: 104 ms (62.39%), Space: 40.4 MB (66.67%) - LeetHub

--- sum-of-subarray-ranges/sum-of-subarray-ranges.js
+++ sum-of-subarray-ranges/sum-of-subarray-ranges.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var subArrayRanges = function(nums) {
-    //写一个二重循环，外层循环枚举子数组的左边界，内层循环枚举子数组的右边界，同时维护当前子数组的最小值和最大值。O(N^2)
+    //写一个二重循环，外层循环枚举子数组的左边界，内层循环枚举子数组的右边界，同时维护当前子数组的最小值和最大值。O(N^2), O(1)
     var sum = 0;
     var max = 0, min = 0;
     for(var i = 0; i < nums.length; i++) {
