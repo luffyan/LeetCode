2f6d355e2c156fd0d9f99e228ef2b8dba4e4f23e
luffyan <ohanran@gmail.com>
2021-12-22 12:30:10 -0800

    Time: 88 ms (85.57%), Space: 46.7 MB (40.68%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.js
+++ valid-palindrome-ii/valid-palindrome-ii.js
@@ -6,6 +6,7 @@ var validPalindrome = function(s) {
     var left = 0, right = s.length-1;
     while(left < right) {
         if(s[left] !== s[right]) {
+            // if we found a pair not equal to each other, check the substring betwn them by removing one of them respectively
             return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
         }
         left++; 
