d8bcbeb669cddff57e8a1905eb839a4615af2a05
luffyan <ohanran@gmail.com>
2021-12-27 15:28:06 -0800

    Time: 516 ms (15.78%), Space: 44 MB (65.31%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.js
+++ merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -13,10 +13,11 @@ var mergeKLists = function(lists) {
     if(lists.length === 0) {
         return null;
     }
-    var minHead = null;
+    var minHead = null;//minHead记录k个list里面最小的head
     var result = null;
-    var tail = null;
-    var count = 0;
+    var tail = null;//一个tail记录result的尾巴
+    var count = 0;//count 记录有几个list已经走到头，是null了
+    //traverse之前先看看有几个list是null
     for(var list of lists) {
         if(list === null) {
             count++;
@@ -24,7 +25,7 @@ var mergeKLists = function(lists) {
     }
 
     while (count !== lists.length) {
-        var idx = -1;
+        var idx = -1;//idx记录那个list拥有最小的head
         var min = Number.MAX_VALUE;
         for(var i = 0; i < lists.length; ++i) {
             if(lists[i] && lists[i].val < min) {
@@ -33,15 +34,16 @@ var mergeKLists = function(lists) {
                 idx = i;
             }
         }
-        lists[idx] = minHead.next;
+        lists[idx] = minHead.next;//最小的head的list更新head， 去掉最小值
+        minHead.next = null;//断掉最小的head
         if(lists[idx] === null) {
             count++;
         }
-        if (result === null) {
-            result = new ListNode(minHead.val);
-            tail = result;
+        if (result === null) {//result是空的时候，result记录头， tail记录尾巴
+            result = minHead;
+            tail = minHead;
         } else {
-            tail.next = new ListNode(minHead.val);
+            tail.next = minHead;
             tail = tail.next;
         }
     }
