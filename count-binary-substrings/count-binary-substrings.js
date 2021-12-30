58ac6c470ae7833734dedfdaa9b014ea28c565b4
luffyan <ohanran@gmail.com>
2021-12-29 22:09:03 -0800

    Time: 130 ms (17.67%), Space: 45.6 MB (30.83%) - LeetHub

--- count-binary-substrings/count-binary-substrings.js
+++ count-binary-substrings/count-binary-substrings.js
@@ -3,20 +3,24 @@
  * @return {number}
  */
 var countBinarySubstrings = function(s) {
+    //相同数目的0和1， 110000答案是1100，and 10,说明两者共同的由少的那个数目的短板决定
+    //
     var ans = 0;
     if (s.length === 1) {return 0;}
+    //记录一样的数字相邻的为一个group,例如【1，1，1，0，0，1，1，1，1，1，0，0，0】 group [3,2,5,3]
     var group = [];
     var count = 1;
     for(var i = 1; i < s.length; i++) {
-        if(s[i] === s[i-1]){//01
+        //current 等于前面一个，count++
+        if(s[i] === s[i-1]){//011
             count++;
-        } else {
+        } else {//current不等于前面一个，把之前的count加到group里面，重新set count等于1
             group.push(count);
             count = 1;
         }
     }
     group.push(count);
-    console.log(group);
+    //console.log(group);
     for(var i = 0; i < group.length-1; i++) {
         ans += Math.min(group[i], group[i+1]);
     }
