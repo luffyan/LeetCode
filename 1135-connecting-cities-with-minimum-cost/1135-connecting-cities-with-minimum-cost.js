c4584db0b68dabffe3cfb90cac76721b7de9f235
luffyan <ohanran@gmail.com>
2022-01-07 12:02:26 -0800

    Time: 182 ms (60.61%), Space: 48.2 MB (46.97%) - LeetHub

---
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var minimumCost = function(n, connections) {
+    var parents = [];
+    var weights = [];
+    for(var i = 1; i <= n; i++) {
+        parents[i] = i;
+        weights[i] = 1;
+    }
+    
+    function union(a, b) {
+        var rootA = find(a);
+        var rootB = find(b);
+        
+        if(rootA === rootB){
+            return;
+        }
+        if(weights[a] > weights[b]) {
+            parents[rootB] = rootA;
+            weights[rootA] += weights[rootB];
+        }else{
+            parents[rootA] = rootB;
+            weights[rootB] += weights[rootA];
+        }
+    }
+    
+    function find(a) {
+        while(a !== parents[a]) {
+            parents[a] = parents[parents[a]];
+            a = parents[a];
+        }
+        return a;
+    }
+    
+    function isConnected(a, b) {
+        return find(a) === find(b);
+    }
+    
+    //sort the array based on cost
+    connections.sort((a, b) => (a[2] - b[2]));
+    
+    //keep track of the total edges to check if all cities are connected in the end
+    var edges = 0;
+    var cost = 0;
+    for(var i = 0; i < connections.length; i++) {
+        var a = connections[i][0];
+        var b = connections[i][1];
+        //do not add the edge from a to b if already connected
+        if(isConnected(a, b)) {
+            continue;
+        }
+        union(a, b);
+        cost += connections[i][2];
+        edges++;
+    }
+    
+    if(edges === n - 1) {
+        return cost;
+    }
+    return -1;
+};
