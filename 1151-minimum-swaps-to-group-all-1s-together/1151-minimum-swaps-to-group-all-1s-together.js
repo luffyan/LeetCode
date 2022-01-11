0ed120f43b4e6e7fff12881344674850689e1487
luffyan <ohanran@gmail.com>
2022-01-11 15:59:10 -0800

    Time: 199 ms (8.08%), Space: 47.1 MB (45.96%) - LeetHub

---
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} data
+ * @return {number}
+ */
+var minSwaps = function(data) {
+    //count how many 1s in the array to get totalOnes
+    var totalOnes = 0;
+    for(var num of data) {
+        if(num === 1) {
+            totalOnes++;
+        }
+    }
+    //6
+    if(totalOnes === 1){
+        return 0;
+    }
+    //sliding window, window size = totalOnes
+    
+    //scan through data array, record current seen ones in the window, 
+    //update most seen ones in the window in the process
+    var left = 0;
+
+    var mostOnes = 0;
+    var currOnes = 0;
+    //[1,0,1,0,1,0,0]
+    for(var i = 0; i < data.length; i++) {
+        if(data[i] === 1) {
+            currOnes++;
+        }
+        if(i - left + 1 === totalOnes) {
+            mostOnes = Math.max(mostOnes, currOnes);
+            
+            if(data[left] === 1) {
+                currOnes--;
+            }
+            left++;
+        }
+    }
+    //#swaps = totalOnes - mostOnes
+    return totalOnes - mostOnes;
+};
