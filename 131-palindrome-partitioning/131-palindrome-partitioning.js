feb381d4155f364df267d6ba4a46e4bbb34f4e28
luffyan <ohanran@gmail.com>
2022-01-14 17:18:36 -0800

    Time: 236 ms (97.38%), Space: 62.1 MB (89.51%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function(s) {
+    // if'a' && 'a' &&'b'
+    // add to result;
+    // if'a' && 'a'
+    // add to result
+    // if'a' && 'a' && 'b'
+    // add to result
+    // 'aa' 
+    
+    var result = [];
+    
+    var findPalindrome = function(list, start) {
+        
+        if(start >= s.length) {
+            result.push(list.slice());
+        }
+        
+        for(var end = start; end < s.length; end++) {
+            if(isPalindrome(start, end)) {
+                list.push(s.substring(start, end+1));
+                findPalindrome(list, end+1);
+                list.pop();
+            }
+            
+        }
+    }
+    var isPalindrome = function(start, end) {
+        while(start < end) {
+            if(s[start] !== s[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    findPalindrome([], 0)
+    return result;
+};
