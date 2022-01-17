b2bde338558f57539660c8f3eff4aa5b1ac3282d
luffyan <ohanran@gmail.com>
2022-01-17 01:21:54 -0800

    Time: 263 ms (6.22%), Space: 43.5 MB (14.49%) - LeetHub

--- 215-kth-largest-element-in-an-arraya-idsolution-btn-215-hrefhttps-labuladonggiteeio-plugin-v3-qno215-and-amp-targetgitee-and-amp-1642410559802-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-215-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/215-kth-largest-element-in-an-arraya-idsolution-btn-215-hrefhttps-labuladonggiteeio-plugin-v3-qno215-and-amp-targetgitee-and-amp-1642410559802-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-215-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
+++ 215-kth-largest-element-in-an-arraya-idsolution-btn-215-hrefhttps-labuladonggiteeio-plugin-v3-qno215-and-amp-targetgitee-and-amp-1642410559802-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-215-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/215-kth-largest-element-in-an-arraya-idsolution-btn-215-hrefhttps-labuladonggiteeio-plugin-v3-qno215-and-amp-targetgitee-and-amp-1642410559802-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-215-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -4,6 +4,17 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    nums.sort((a, b) => (b-a));
-    return nums[k-1];
+    // nums.sort((a, b) => (b-a));
+    // return nums[k-1];
+    //use a max pq to maintain a k elements array
+    
+    var pq = new MinPriorityQueue();
+    for(var num of nums) {
+        pq.enqueue(num);
+        if(pq.size() > k) {
+            pq.dequeue().element;
+        }
+    }
+    
+    return pq.dequeue().element;
 };
