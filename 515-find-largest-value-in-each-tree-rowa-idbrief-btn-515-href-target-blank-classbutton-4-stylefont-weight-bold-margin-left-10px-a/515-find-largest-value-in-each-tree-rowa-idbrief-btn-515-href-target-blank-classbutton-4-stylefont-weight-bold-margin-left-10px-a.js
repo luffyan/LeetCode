e1814fb39c5bbd819f5cfb7aa80466fbfc430cc1
luffyan <ohanran@gmail.com>
2022-01-17 00:24:06 -0800

    Time: 84 ms (92.12%), Space: 43.5 MB (70.12%) - LeetHub

--- 515-find-largest-value-in-each-tree-rowa-idbrief-btn-515-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/515-find-largest-value-in-each-tree-rowa-idbrief-btn-515-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
+++ 515-find-largest-value-in-each-tree-rowa-idbrief-btn-515-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/515-find-largest-value-in-each-tree-rowa-idbrief-btn-515-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -11,24 +11,42 @@
  * @return {number[]}
  */
 var largestValues = function(root) {
-    //bfs use queue to store node on the same level
-    var result = [];
-    var queue = [];
+//     //bfs use queue to store node on the same level
+//     var result = [];
+//     var queue = [];
     
-    if(root === null) {
-        return result;
-    }
-    queue.push(root);
-    while(queue.length !== 0) {
-        var len = queue.length;
-        var max = Number.MIN_SAFE_INTEGER;
-        for(var i = 0; i < len; i++) {
-            var currNode = queue.shift();
-            if(currNode.left) queue.push(currNode.left);
-            if(currNode.right) queue.push(currNode.right);
-            max = Math.max(max, currNode.val);
+//     if(root === null) {
+//         return result;
+//     }
+//     queue.push(root);
+//     while(queue.length !== 0) {
+//         var len = queue.length;
+//         var max = Number.MIN_SAFE_INTEGER;
+//         for(var i = 0; i < len; i++) {
+//             var currNode = queue.shift();
+//             if(currNode.left) queue.push(currNode.left);
+//             if(currNode.right) queue.push(currNode.right);
+//             max = Math.max(max, currNode.val);
+//         }
+//         result.push(max);
+//     }
+//     return result;
+    var result = [];
+    
+    //record max value for each depth
+    var dfs = function(node, depth) {
+        if(node === null) {
+            return;
         }
-        result.push(max);
+        if(result[depth] === undefined) {
+            result[depth] = node.val;
+        }else{
+            result[depth] = Math.max(node.val, result[depth]);
+        }
+        
+        dfs(node.left, depth+1);
+        dfs(node.right, depth+1);
     }
+    dfs(root, 0);
     return result;
 };
