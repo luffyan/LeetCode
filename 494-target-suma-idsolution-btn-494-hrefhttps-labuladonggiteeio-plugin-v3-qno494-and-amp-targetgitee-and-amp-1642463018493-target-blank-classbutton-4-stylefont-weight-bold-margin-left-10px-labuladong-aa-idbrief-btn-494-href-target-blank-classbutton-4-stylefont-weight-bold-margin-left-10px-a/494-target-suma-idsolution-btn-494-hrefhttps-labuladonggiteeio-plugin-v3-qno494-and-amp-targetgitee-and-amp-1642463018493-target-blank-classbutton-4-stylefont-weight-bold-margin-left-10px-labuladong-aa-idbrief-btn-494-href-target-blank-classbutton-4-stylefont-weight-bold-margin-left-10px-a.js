a8415f72827914c359f8a955078e709b86fece11
luffyan <ohanran@gmail.com>
2022-01-18 23:43:15 -0800

    Time: 405 ms (58.20%), Space: 49.9 MB (14.64%) - LeetHub

--- 494-target-suma-idsolution-btn-494-hrefhttps-labuladonggiteeio-plugin-v3-qno494-and-amp-targetgitee-and-amp-1642463018493-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-494-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/494-target-suma-idsolution-btn-494-hrefhttps-labuladonggiteeio-plugin-v3-qno494-and-amp-targetgitee-and-amp-1642463018493-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-494-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
+++ 494-target-suma-idsolution-btn-494-hrefhttps-labuladonggiteeio-plugin-v3-qno494-and-amp-targetgitee-and-amp-1642463018493-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-494-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/494-target-suma-idsolution-btn-494-hrefhttps-labuladonggiteeio-plugin-v3-qno494-and-amp-targetgitee-and-amp-1642463018493-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-494-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -4,20 +4,45 @@
  * @return {number}
  */
 var findTargetSumWays = function(nums, target) {
-    //iterate through the nums array
     //for current number, either add it or subtract it
+    //不用for loop
+    // var count = 0;
+    // var findTarget = function(sum, currIdx) {
+    //     if(currIdx === nums.length){
+    //         if(sum === target) {
+    //             count++;
+    //         }
+    //         return;
+    //     } else {
+    //         if(memo[sum,currIdx]) {
+    //             return
+    //         }
+    //     }
+    //     findTarget(sum + nums[currIdx], currIdx+1);
+    //     findTarget(sum - nums[currIdx], currIdx+1);
+    // }
+    // findTarget(0, 0);
+    // return count;
     
-    var count = 0;
-    var findTarget = function(sum, currIdx) {
-        if(currIdx === nums.length){
-            if(sum === target) {
-                count++;
-            }
-            return;
+    //------------------------------------加memo---------------------------------------
+    // var count = 0;
+    var memo = {};
+    var findCombos = function(sum, idx) {
+        var key = sum.toString() + ":" + idx.toString();
+        if (key in memo) {
+            return memo[key];
         }
-        findTarget(sum + nums[currIdx], currIdx+1);
-        findTarget(sum - nums[currIdx], currIdx+1);
+        if(idx === nums.length) {
+            if(sum === target) {
+                return 1;
+            }
+            return 0;
+            // return sum;
+        } else {
+            var count = findCombos(sum + nums[idx], idx+1) + findCombos(sum - nums[idx], idx+1);
+            memo[key] = count;
+            return count;
+        }
     }
-    findTarget(0, 0);
-    return count;
+    return findCombos(0, 0);
 };
