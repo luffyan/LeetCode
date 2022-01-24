949e08741b244d31403f075f5f6d0127ca2cbf2d
luffyan <ohanran@gmail.com>
2022-01-23 17:47:15 -0800

    Time: 1424 ms (5.01%), Space: 79.5 MB (51.69%) - LeetHub

--- 204-count-primesa-idsolution-btn-204-hrefhttps-labuladonggiteeio-plugin-v3-qno204-and-amp-targetgitee-and-amp-1642845250128-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-204-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/204-count-primesa-idsolution-btn-204-hrefhttps-labuladonggiteeio-plugin-v3-qno204-and-amp-targetgitee-and-amp-1642845250128-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-204-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
+++ 204-count-primesa-idsolution-btn-204-hrefhttps-labuladonggiteeio-plugin-v3-qno204-and-amp-targetgitee-and-amp-1642845250128-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-204-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/204-count-primesa-idsolution-btn-204-hrefhttps-labuladonggiteeio-plugin-v3-qno204-and-amp-targetgitee-and-amp-1642845250128-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-204-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -22,13 +22,13 @@ var countPrimes = function(n) {
         }
     }
     var count = 0;
-    for(var i = 0; i < array.length; i++) {
+    for(var i = 2; i < array.length; i++) {
         if(array[i]) {
             count++;
         }
     }
     
-    return count-2;
+    return count;
     
     
     
