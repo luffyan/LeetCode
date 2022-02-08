6d999f4b5e16d22ff6460b918ed3d0031d4ee7a4
luffyan <ohanran@gmail.com>
2022-02-07 20:57:39 -0800

    Time: 79 ms (67.15%), Space: 42.2 MB (13.95%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -11,13 +11,16 @@ var search = function(nums, target) {
         var mid = Math.floor(start + (end-start)/2);
         if(nums[mid] === target) {
             return mid;
-        } else if(nums[mid] >= nums[start]) {
+        } else if(nums[mid] >= nums[start]) {//mid is in non-rotated array
+            //if target is in the non-rotated array
             if(target >= nums[start] && target <= nums[mid]) {
                 end = mid;
             } else {
                 start = mid;
             }
         } else {
+            //if mid is in rotated array
+            //if target is in nonrotated part of the array
             if(target >= nums[mid] && target <= nums[end]) {
                 start = mid;
             } else {
