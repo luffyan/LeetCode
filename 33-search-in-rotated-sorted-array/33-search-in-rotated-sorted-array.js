67b454bb00f562a3628b231d69be0c1257564e2a
luffyan <ohanran@gmail.com>
2022-02-07 20:27:51 -0800

    Time: 101 ms (40.38%), Space: 42.6 MB (6.38%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    var start = 0;
+    var end = nums.length-1;
+    
+    while(start+1 < end) {
+        var mid = Math.floor(start + (end-start)/2);
+        if(nums[mid] === target) {
+            return mid;
+        } else if(nums[mid] >= nums[start]) {
+            if(target >= nums[start] && target <= nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        } else {
+            if(target >= nums[mid] && target <= nums[end]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+    }
+    if(nums[start] === target) {
+        return start;
+    }else if(nums[end] === target) {
+        return end;
+    }else {
+        return -1;
+    }
+    
+};
