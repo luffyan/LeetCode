0284acc37ddda3c4eab60fcfb208b822924cdb97
luffyan <ohanran@gmail.com>
2022-02-07 17:48:31 -0800

    Time: 1098 ms (17.89%), Space: 82.8 MB (38.17%) - LeetHub

---
+++ 204-count-primesa-idsolution-btn-204-hrefhttps-labuladonggiteeio-plugin-v3-qno204-and-amp-targetgitee-and-amp-1644284177913-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-204-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/204-count-primesa-idsolution-btn-204-hrefhttps-labuladonggiteeio-plugin-v3-qno204-and-amp-targetgitee-and-amp-1644284177913-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-204-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -0,0 +1,79 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    
+    //1 is not a prime
+    //[true,true, true, true,true]
+    //[0,1,2,3,4]
+    //create an array of size n, idx: 0,1,2,3...n-1, idx[0]=1, idx[1]= 2, ...idx[n-1] = n;
+    //find all non prime numbers from 2 to n;
+    //iterate from i = 2...sqrt of n;
+    //i = 2; 4,6,8,10, until number is > n, mark those positions false;
+    //i = 3; 
+    if(n <= 2) return 0;
+    
+    var array = new Array(n);
+    array.fill(true);
+    // for (var i =0; i<n; i++) {
+    //     array[i] = false;
+    // }
+    for(var i = 2; i * i < n; i++) {
+        for(var j = i*i; j < n; j+=i) {
+            array[j] = false;
+        }
+    }
+    var count = 0;
+    //console.log("size: ", array.length);
+    for(var i = 2; i < n; i++) {
+        //console.log("i:", i, " array[i]:", array[i]);
+        if(array[i]) {
+            count++;
+        }
+    }
+    
+    return count;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     if (n <= 2) return 0;
+    
+//     var numbers = [];
+//     for(var i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+//         if(!numbers[i]) {
+//             for(var j = i*i; j < n; j += i) {
+//                 numbers[j] = true;
+//                 console.log(j, numbers[j]);
+//             }
+//         }
+//     }
+//     var numberOfPrimes = 0;
+//     for(var i = 2; i < n; i++) {
+//         if(!numbers[i]) {
+//             numberOfPrimes++;
+//         }
+//     }
+//     return numberOfPrimes;
+};
