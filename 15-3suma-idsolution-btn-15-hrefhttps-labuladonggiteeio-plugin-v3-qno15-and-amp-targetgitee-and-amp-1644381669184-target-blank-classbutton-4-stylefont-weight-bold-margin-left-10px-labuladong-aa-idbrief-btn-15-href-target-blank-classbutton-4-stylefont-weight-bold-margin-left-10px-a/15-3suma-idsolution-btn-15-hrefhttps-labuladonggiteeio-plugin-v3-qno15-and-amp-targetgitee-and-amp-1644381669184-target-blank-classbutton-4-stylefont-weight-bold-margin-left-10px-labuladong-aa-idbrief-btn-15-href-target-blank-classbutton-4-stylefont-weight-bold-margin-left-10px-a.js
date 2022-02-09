eb7bf5567c50ace3dacd8fca53b77f655e3a7d2a
luffyan <ohanran@gmail.com>
2022-02-08 22:37:06 -0800

    Time: 624 ms (22.23%), Space: 60.2 MB (13.66%) - LeetHub

--- 15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644381669184-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644381669184-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
+++ 15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644381669184-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644381669184-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -23,23 +23,45 @@ var threeSum = function(nums) {
 };
 
  //find two numbers sum to target, add them to result array
-    var twoSum = function(target, start, nums, result) {
-        var left = start, right = nums.length-1;
-        while(left < right) {
-            var sum = nums[left] + nums[right];
-            if(sum === target) {
-                result.push([nums[start-1], nums[left], nums[right]]);
-                left++;
-                right--;
-                //只需要make sure 其中的一个不需要重复即可
-                while (nums[left] == nums[left-1]) left++;
-                //while (left < right && nums[right] == nums[right+1]) right--;
-            } else if(sum > target) {//[1,2,3,3,3,3]
-                right--;
-            } else {
-                left++;
-            }
+//     var twoSum = function(target, start, nums, result) {
+//         var left = start, right = nums.length-1;
+//         while(left < right) {
+//             var sum = nums[left] + nums[right];
+//             if(sum === target) {
+//                 result.push([nums[start-1], nums[left], nums[right]]);
+//                 left++;
+//                 right--;
+//                 //只需要make sure 其中的一个不需要重复即可
+//                 while (nums[left] == nums[left-1]) left++;
+//                 //while (left < right && nums[right] == nums[right+1]) right--;
+//             } else if(sum > target) {//[1,2,3,3,3,3]
+//                 right--;
+//             } else {
+//                 left++;
+//             }
             
+//         }
+//     }
+
+var twoSum = function(complement, start, nums, result) {
+    var numSet = new Set();
+    //iterate through the array, if the complement-current number is in the map, 
+        //yes->add to result, remove from set
+        //no add current number to map
+    //[-2,0,1,1,2]
+    //-2 comp = 2
+    //set=[0]
+    for(var i = start; i < nums.length; i++) {
+        var comp = complement - nums[i];
+        
+        if(numSet.has(comp)) {
+            result.push([nums[start-1], comp, nums[i]]);
+            numSet.delete(comp);
+            while(nums[i+1] == nums[i] && i+1 < nums.length){
+                i++;
+            }
+        }else{
+            numSet.add(nums[i]);
         }
     }
-
+}
