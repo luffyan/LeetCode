86d89f06e652748d249b3797f5e4c30109549333
luffyan <ohanran@gmail.com>
2022-02-08 20:44:57 -0800

    Time: 227 ms (57.18%), Space: 51.5 MB (41.03%) - LeetHub

---
+++ 15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644381669184-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/15-3suma-idsolution-btn-15-hrefhttps-labuladonggiteeio-plugin-v3-qno15-and-amp-targetgitee-and-amp-1644381669184-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-15-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    if(nums.length < 3) return [];
+    //sort and two pointers
+    //sort the array to remove dup
+    //first for loop to choose the first number i;
+        //iterate from i+1 to the end of the array, use two sum helper function, 
+        //sum = target - first number at i;
+        //for the next iteration, skip the same value 
+    var result = [];
+    nums.sort((a, b) => (a-b));
+    
+    for(var i = 0; i < nums.length; i++) {
+        if(i === 0 || nums[i-1] !== nums[i]) {
+            var complement = 0-nums[i];
+            twoSum(complement, i+1, nums, result);
+        }
+    }
+    return result;
+};
+
+ //find two numbers sum to target, add them to result array
+    var twoSum = function(target, start, nums, result) {
+        var left = start, right = nums.length-1;
+        while(left < right) {
+            var sum = nums[left] + nums[right];
+            if(sum === target) {
+                result.push([nums[start-1], nums[left], nums[right]]);
+                left++;
+                right--;
+                //只需要make sure 其中的一个不需要重复即可
+                while (nums[left] == nums[left-1]) left++;
+                //while (left < right && nums[right] == nums[right+1]) right--;
+            } else if(sum > target) {//[1,2,3,3,3,3]
+                right--;
+            } else {
+                left++;
+            }
+            
+        }
+    }
+
