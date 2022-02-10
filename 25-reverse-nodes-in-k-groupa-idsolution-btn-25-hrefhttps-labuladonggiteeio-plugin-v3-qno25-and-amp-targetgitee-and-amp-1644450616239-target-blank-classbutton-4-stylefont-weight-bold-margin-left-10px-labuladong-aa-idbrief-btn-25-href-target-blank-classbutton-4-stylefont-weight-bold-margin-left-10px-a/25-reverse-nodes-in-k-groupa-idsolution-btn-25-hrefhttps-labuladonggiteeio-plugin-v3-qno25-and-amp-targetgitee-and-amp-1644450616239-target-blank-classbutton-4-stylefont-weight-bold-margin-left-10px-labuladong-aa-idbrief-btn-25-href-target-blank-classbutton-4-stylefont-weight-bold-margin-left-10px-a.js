a1b1ad4ac1b62b378adc97fd98d536937b9cdc06
luffyan <ohanran@gmail.com>
2022-02-09 16:17:42 -0800

    Time: 84 ms (93.76%), Space: 45.2 MB (15.47%) - LeetHub

---
+++ 25-reverse-nodes-in-k-groupa-idsolution-btn-25-hrefhttps-labuladonggiteeio-plugin-v3-qno25-and-amp-targetgitee-and-amp-1644450616239-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-25-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/25-reverse-nodes-in-k-groupa-idsolution-btn-25-hrefhttps-labuladonggiteeio-plugin-v3-qno25-and-amp-targetgitee-and-amp-1644450616239-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-25-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    var curr = head;
+    var ktail = null;
+    
+    var new_head = null;
+    
+    while(curr) {
+        var count = 0;
+        curr = head;//?
+        while(count < k && curr) {//curr指向要翻转的后一个node
+            curr = curr.next;
+            count++;
+        }
+        
+        if(count >= k) {
+            var revHead = reverseKNodes(head, k);
+            if(new_head === null) {
+                new_head = revHead;
+            }
+            
+            if(ktail !== null) {
+                ktail.next = revHead;
+            }
+            
+            ktail = head;
+            head = curr;
+        }
+    }
+    if(ktail !== null) {
+        ktail.next = head;
+    }
+    return new_head === null ? head : new_head;
+};
+
+var reverseKNodes = function(head, k) {
+    //reverse k nodes of the given linked list
+    //assumes list contains at least k node, otherwise return null
+    if(k < 2) {
+        return head;
+    }
+    var prev = new ListNode(-1, null);
+    var current = head;
+    
+    while(k > 0) {
+        //temp store next node
+        var next = current.next;
+        //reverse
+        current.next = prev;
+        //update
+        prev = current;
+        current = next;
+        k--;
+    }
+    //return the head of the reversed list
+    return prev;
+}
