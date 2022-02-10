ef46f48eb91eb55cb893e761232119789780f378
luffyan <ohanran@gmail.com>
2022-02-09 21:13:10 -0800

    Time: 111 ms (59.26%), Space: 51.4 MB (20.44%) - LeetHub

---
+++ 124-binary-tree-maximum-path-suma-idbrief-btn-124-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/124-binary-tree-maximum-path-suma-idbrief-btn-124-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.js
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    //max sum = root value + max of left + max of right
+    //postorder traversal, calculate current sum for current node
+    //update max sum
+    var max = Number.NEGATIVE_INFINITY;
+    var findDepthSum = function(node) {
+        if(node === null) {
+            return 0;
+        }
+        var left = Math.max(findDepthSum(node.left), 0);
+        var right = Math.max(findDepthSum(node.right), 0);
+        
+        var currSum = left + right + node.val;
+        max = Math.max(currSum, max);
+        
+        return Math.max(left, right) + node.val;
+    }
+    findDepthSum(root);
+    return max;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+let max = Number.NEGATIVE_INFINITY;
+    const findSum = function (node) {
+        if(!node) {
+            return 0;
+        }
+        
+        let left = Math.max(findSum(node.left), 0);
+        let right = Math.max(findSum(node.right), 0);
+        
+        let currSum = node.val + left + right;
+        max = Math.max(currSum, max);
+        
+        return node.val + Math.max(left, right);
+    }
+    findSum(root)
+    return max;
+*/
